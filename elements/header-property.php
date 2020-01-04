7f929dab16f9442ce7498b49f5c56d4273378a5c
Burnoutcat (J. Zheng) <jackyzhengjian@gmail.com>
2020-01-03 16:43:00 -0800

    Added site structure

---
+++ elements/header-property.php
@@ -0,0 +1,9 @@
+<meta name="viewport" content="width=device-width">
+<link href="https://fonts.googleapis.com/css?family=Heebo:400,700,900|Lato:300i,400&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="common/common.css">
+<link rel="stylesheet" href="css/site.css">
+<link rel="stylesheet" href="css/style.css">
+
+
+
